ed86fe669221b09817b643fa21f9b71b5424f30a
borko1945 <borko1945@mail.bg>
2018-02-28 22:01:08 +0200

    parser is complete

--- parser/index.js
+++ parser/index.js
@@ -2,27 +2,51 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
+  var result = [];
   try {
     await driver.get('http://www.pvc-mashini.com/catalog/');
-    var urlWebElements = await driver.findElements(By.className('portfoliolink'));
-    // for(let element of urlWebElements) {
-    processPage(await urlWebElements[0].getAttribute("href"));
-    // }
+    var localizedUrls = await getLocalizedUrls(driver);
+    for(localizedPageUrl of localizedUrls){
+      let localizedPageDriver = await new Builder().forBrowser('chrome').build();
+
+      await localizedPageDriver.get(localizedPageUrl);
+      var urlWebElements = await localizedPageDriver.findElements(By.className('portfoliolink'));
+      for (let element of urlWebElements) {
+        result.push(await processPage(await element.getAttribute("href")));
+      }
+
+      await localizedPageDriver.quit();
+    }
   } finally {
     await driver.quit();
+    console.log(JSON.stringify(result));
   }
 })();
 
+getLocalizedUrls = async function (driver) {
+  var localizedUrls = [];
+  var imageElements = await driver.findElements(By.css("#nav-main a img"));
+  for (imageElement of imageElements) {
+    var aElement = imageElement.findElement(By.xpath(".."));
+    localizedUrls.push(await aElement.getAttribute("href"));
+  }
+
+  return localizedUrls;
+}
+
 processPage = async function (url) {
+  var item = {};
   let driver = await new Builder().forBrowser('chrome').build();
 
   await driver.get(url);
-  let decrtiption = await parseDescription(driver);
-  let techDetails = await parseTechnicalDetails(driver);
+  item.title = await parseTitle(driver);
+  item.imageUrl = await parseImageUrl(driver);
+  item.decription = await parseDescription(driver);
+  item.technicalDetails = await parseTechnicalDetails(driver);
 
   await driver.quit();
-  console.log(JSON.stringify(decrtiption, null, '  '));
-  console.log(JSON.stringify(techDetails, null, '  '));
+
+  return item;
 }
 
 parseDescription = async function (driver) {
@@ -41,7 +65,7 @@ parseTechnicalDetails = async function (driver) {
   for (tr of trList) {
     let tdList = await tr.findElements(By.xpath(".//*"));
     let technicalDetailsElement = {};
-    
+
     technicalDetailsElement.key = await tdList[0].getText();
     technicalDetailsElement.value = await tdList[1].getText();
 
@@ -49,4 +73,14 @@ parseTechnicalDetails = async function (driver) {
   }
 
   return descriptionDetails;
+}
+
+parseTitle = async function (driver) {
+  let titleElement = await driver.findElement(By.css(".entry-title"));
+  return await titleElement.getText();
+}
+
+parseImageUrl = async function (driver) {
+  let imageElement = await driver.findElement(By.css(".portfolio-single-img img"));
+  return imageElement.getAttribute("src");
 }
