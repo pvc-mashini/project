eef15fc35bfb2566ec9d3deac204427b839ce4d4
Aylin Hyumet <lina82@abv.bg>
2018-03-18 22:24:09 +0200

    Added new logo in the navigation bar

--- PVC/machines.js
+++ PVC/machines.js
@@ -40,7 +40,7 @@ function renderHeader() {
 				    	<span class="icon-bar"></span>
 				    	<span class="icon-bar"></span>
 					</button>	
-					<a class="navbar-brand" href="#">Acme Web Design</a>		    		
+					<a class="navbar-brand" href="#"><img id="logo" src="images/logo.svg"></a>
 				</div>
 				<div class="collapse navbar-collapse" id="MyNavbar">	
 					<ul class="nav navbar-nav navbar-right pull-right">
